23d65c4369bd3558ad4b1e6a39026464a828360b
Karlo Čeh <karlo.ceh1@gmail.com>
2021-08-27 14:21:14 +0200

    DI with hilt update

---
+++ SpotifyClone/app/src/main/java/com/samples/spotifyclone/SpotifyApplication.kt
@@ -0,0 +1,9 @@
+package com.samples.spotifyclone
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SpotifyApplication : Application(){
+
+}
--- SpotifyClone/app/src/main/java/com/samples/spotifyclone/ui/MainActivity.kt
+++ SpotifyClone/app/src/main/java/com/samples/spotifyclone/ui/MainActivity.kt
@@ -2,9 +2,17 @@ package com.samples.spotifyclone.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.RequestManager
 import com.samples.spotifyclone.R
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var glide: RequestManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
