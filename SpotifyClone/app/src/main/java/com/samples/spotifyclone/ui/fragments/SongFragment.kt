dab8ed3a11a016b2581be85cfedf018e733ac7ad
Karlo Čeh <karlo.ceh1@gmail.com>
2021-09-01 10:20:19 +0200

    final update for spotify clone

--- SpotifyClone/app/src/main/java/com/samples/spotifyclone/ui/fragments/SongFragment.kt
+++ SpotifyClone/app/src/main/java/com/samples/spotifyclone/ui/fragments/SongFragment.kt
@@ -1,19 +1,24 @@
 package com.samples.spotifyclone.ui.fragments
 
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
 import android.view.View
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.RequestManager
 import com.samples.spotifyclone.R
 import com.samples.spotifyclone.data.entities.Song
+import com.samples.spotifyclone.exoplayer.isPlaying
 import com.samples.spotifyclone.exoplayer.toSong
 import com.samples.spotifyclone.ui.viewmodels.MainViewModel
 import com.samples.spotifyclone.ui.viewmodels.SongViewModel
 import com.samples.spotifyclone.util.Status
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_song.*
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -27,10 +32,48 @@ class SongFragment : Fragment(R.layout.fragment_song) {
 
     private var currPlayingSong: Song? = null
 
+    private var playbackState : PlaybackStateCompat? = null
+
+    private var shouldUpdateSeekbar = true
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
         subscribeToObservers()
+
+        ivPlayPauseDetail.setOnClickListener {
+            currPlayingSong?.let {
+                mainViewModel.playOrToggleSong(it, true)
+            }
+        }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if(fromUser){
+                    setCurrPlayerTimeToTextView(progress.toLong())
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                shouldUpdateSeekbar = false
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                seekBar?.let {
+                    mainViewModel.seekTo(it.progress.toLong())
+                    shouldUpdateSeekbar = true
+                }
+            }
+
+        })
+
+        ivSkipPrevious.setOnClickListener {
+            mainViewModel.skipToPreviousSong()
+        }
+
+        ivSkip.setOnClickListener {
+            mainViewModel.skipToNextSong()
+        }
     }
 
     private fun updateTitleAndSongImage(song: Song){
@@ -61,5 +104,31 @@ class SongFragment : Fragment(R.layout.fragment_song) {
             currPlayingSong = it.toSong()
             updateTitleAndSongImage(currPlayingSong!!)
         }
+
+        mainViewModel.playbackState.observe(viewLifecycleOwner){
+            playbackState = it
+            ivPlayPauseDetail.setImageResource(
+                if(playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+            )
+            seekBar.progress = it?.position?.toInt() ?: 0
+        }
+
+        songViewModel.currentPlayerPosition.observe(viewLifecycleOwner){
+            if(shouldUpdateSeekbar){
+                seekBar.progress = it.toInt()
+                setCurrPlayerTimeToTextView(it)
+            }
+        }
+
+        songViewModel.currentSongDuration.observe(viewLifecycleOwner){
+            seekBar.max = it.toInt()
+            val dateFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+            tvSongDuration.text = dateFormat.format(it)
+        }
+    }
+
+    private fun setCurrPlayerTimeToTextView(ms: Long) {
+        val dateFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+        tvCurTime.text = dateFormat.format(ms)
     }
 }
