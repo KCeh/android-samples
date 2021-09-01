d7e830c26c80cb8b5bb9d4306d80116f219f81be
Karlo Čeh <karlo.ceh1@gmail.com>
2021-09-01 08:59:38 +0200

    main activity VM implemented

---
+++ SpotifyClone/app/src/main/java/com/samples/spotifyclone/exoplayer/MediaMetadataCompatExt.kt
@@ -0,0 +1,14 @@
+package com.samples.spotifyclone.exoplayer
+
+import android.support.v4.media.MediaMetadataCompat
+import com.samples.spotifyclone.data.entities.Song
+
+fun MediaMetadataCompat.toSong() : Song?{
+    return description?.let {
+        Song(it.mediaId ?: "",
+            it.title.toString(),
+            it.subtitle.toString(),
+            it.mediaUri.toString(),
+            it.iconUri.toString())
+    }
+}
--- SpotifyClone/app/src/main/java/com/samples/spotifyclone/ui/MainActivity.kt
+++ SpotifyClone/app/src/main/java/com/samples/spotifyclone/ui/MainActivity.kt
@@ -1,20 +1,118 @@
 package com.samples.spotifyclone.ui
 
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.RequestManager
+import com.google.android.material.snackbar.Snackbar
 import com.samples.spotifyclone.R
+import com.samples.spotifyclone.adapters.SwipeSongAdapter
+import com.samples.spotifyclone.data.entities.Song
+import com.samples.spotifyclone.exoplayer.isPlaying
+import com.samples.spotifyclone.exoplayer.toSong
+import com.samples.spotifyclone.ui.viewmodels.MainViewModel
+import com.samples.spotifyclone.util.Status
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val mainViewModel: MainViewModel by viewModels()
+
+    @Inject
+    lateinit var swipeSongAdapter: SwipeSongAdapter
+
     @Inject
     lateinit var glide: RequestManager
+
+    private var currPlayingSong : Song? = null
+
+    private var playbackState: PlaybackStateCompat? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        subscribeToObservers()
+
+        vpSong.adapter = swipeSongAdapter
+
+        vpSong.registerOnPageChangeCallback(object  : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if(playbackState?.isPlaying == true){
+                    mainViewModel.playOrToggleSong(swipeSongAdapter.songs[position])
+                }else{
+                    currPlayingSong = swipeSongAdapter.songs[position]
+                }
+            }
+        })
+
+        ivPlayPause.setOnClickListener {
+            currPlayingSong?.let {
+                mainViewModel.playOrToggleSong(it, true)
+            }
+        }
+    }
+
+    private fun switchViewPagerToCurrentSong(song: Song){
+        val newItemIndex = swipeSongAdapter.songs.indexOf(song)
+        if(newItemIndex != -1){
+            vpSong.currentItem = newItemIndex
+            currPlayingSong = song
+        }
+    }
+
+    private fun subscribeToObservers(){
+        mainViewModel.mediaItems.observe(this){
+            it?.let { result ->
+                when(result.status){
+                    Status.SUCCESS -> {
+                        result.data?.let { songs ->
+                            swipeSongAdapter.songs = songs
+                            if(songs.isNotEmpty()){
+                                glide.load((currPlayingSong ?: songs[0]).imageUrl).into(ivCurSongImage)
+                            }
+
+                            switchViewPagerToCurrentSong(currPlayingSong ?: return@observe)
+                        }
+                    }
+                    Status.ERROR -> Unit
+                    Status.LOADING -> Unit
+                }
+            }
+        }
+
+        mainViewModel.currPlayingSong.observe(this){
+            if(it==null) return@observe
+            currPlayingSong = it.toSong()
+            glide.load(currPlayingSong?.imageUrl).into(ivCurSongImage)
+            switchViewPagerToCurrentSong(currPlayingSong ?: return@observe)
+        }
+
+        mainViewModel.playbackState.observe(this){
+            playbackState = it
+            ivPlayPause.setImageResource(
+                if(playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+            )
+        }
+
+        mainViewModel.isConnected.observe(this){
+            it?.getContentIfNotHandled()?.let { result ->
+                when(result.status){
+                    Status.SUCCESS -> Unit
+                    Status.ERROR -> {
+                        Snackbar.make(rootLayout,
+                            result.message ?: "Unknown error",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                    Status.LOADING -> Unit
+                }
+            }
+        }
     }
 }
